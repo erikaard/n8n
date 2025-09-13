614160c59d73eb2ce989d4783c2fbe985e1376b0
Erik Aardal <57392926+erikaard@users.noreply.github.com>
2025-09-13 18:24:45 +0200

    Update copy-templates.mjs

--- packages/@n8n/node-cli/scripts/copy-templates.mjs
+++ packages/@n8n/node-cli/scripts/copy-templates.mjs
@@ -1,6 +1,11 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
+#!/usr/bin/env node
 
+import glob from 'fast-glob';
+import { cp } from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Emulate __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -9,3 +14,9 @@ const templateFiles = glob.sync(['src/template/templates/**/*'], {
   ignore: ['**/node_modules', '**/dist'],
   dot: true,
 });
+
+await Promise.all(
+  templateFiles.map((template) =>
+    cp(template, `dist/${template.replace('src/', '')}`, { recursive: true }),
+  ),
+);
